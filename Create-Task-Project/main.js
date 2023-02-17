c71093af822724891ca3e093554e7d0263f233e7
4rham <95187191+4rham@users.noreply.github.com>
2023-02-17 10:14:30 -0500

    hgarrh

--- Create-Task-Project/main.js
+++ Create-Task-Project/main.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const URL = "https://pokeapi.co/api/v2/";
+
+window.onload = () => {
+  random();
+};
+const random = () => {
+  console.log(fetch(URL));
+  .then((response) => {
+    console.log(response)
+    console.log(response.json()
+  })
+};
