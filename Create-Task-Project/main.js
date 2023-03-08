101baf7a89fe901fbf633686ae756abf67814756
4rham <arhambajwa24@gmail.com>
2023-03-08 01:40:19 -0500

    ochen khorosho

--- Create-Task-Project/main.js
+++ Create-Task-Project/main.js
@@ -1,22 +1,46 @@
 import "./style.css";
 
-const URL = "https://pokeapi.co/api/v2/pokemon?limit=1010";
+const URL = `https://pokeapi.co/api/v2/pokemon/`;
+const start = document.querySelector("#start") 
+const display = document.querySelector("#guess")
+const number = () => Math.floor(Math.random() * 1010 + 1)
+let blank = null;
+
+const random = () => {
+  const fullurl = URL + number()
+  fetch(fullurl)
+  .then((data) =>  data.json())
+  .then((pokemon) => 
+    present(pokemon.name, pokemon.sprites.front_default))
+};
+
+const present = (name, sprite) => {
+  const fun = `<h2 class = "species">${name}</h2>
+  <img class = "mon" src = "${sprite}" alt = "${name}">`;
+  display.innerHTML = fun
+  console.log(name, sprite)
+}
+
+function bk() {
+  blank = display.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter: "_")).join('')
+  document.getElementById('species').innerHTML = blank
+}
+
+start.addEventListener("click", random)
 
 window.onload = () => {
   random()
 }
 
-const random = () => {
-  fetch(URL)
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data)
-  })
-};
-
-random = (pokemon) => {
-  document.getElementById("prompt").innerText = `${pokemon.results[0].name}`
-  document.getElementById("img").innerText = `${pokemon.results[0].url.sprites["front_default"]}`
+function letters() {
+  let key = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `<button class = "bn" id = `+ letter +` onClick = "handleGuess('`+ letter +`')">
+    ` + letter + `
+    </button> 
+    `).join(''); 
+  
+  document.getElementById('board').innerHTML = key
 }
+
+letters();
+bk();
